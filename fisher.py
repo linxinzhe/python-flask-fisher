b6829467fb8313b136c3ff6f2653a283b0dc34fa
linxinzhe <linxinzhechn@qq.com>
2019-04-02 23:07:44 +0800

    Initial commit

---
+++ fisher.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+__author__='linxinzhe'
+
+@app.route('/hello/')  # 唯一url原则，带/可以兼容不带/情况
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
