bad4b3c9567a336aa6eefdeaa21179a1d45bb090
linxinzhe <linxinzhechn@qq.com>
2019-04-05 21:06:21 +0800

    注册路由

--- fisher.py
+++ fisher.py
@@ -2,12 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-__author__='linxinzhe'
+__author__ = 'linxinzhe'
 
-@app.route('/hello/')  # 唯一url原则，带/可以兼容不带/情况
-def hello_world():
+
+# @app.route('/hello/')  # 唯一url原则，带/可以兼容不带/情况
+def hello():
     return 'Hello World!'
 
+
+app.add_url_rule('/hello/', view_func=hello)  # 另一个注册路由方式
 
 if __name__ == '__main__':
     app.run(debug=True)
