fe50abf06d12054216c9210e239daca03d44a5bb
linxinzhe <linxinzhechn@qq.com>
2019-04-06 09:28:02 +0800

    配置文件

--- fisher.py
+++ fisher.py
@@ -1,16 +1,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 __author__ = 'linxinzhe'
 
 
-# @app.route('/hello/')  # 唯一url原则，带/可以兼容不带/情况
+@app.route('/hello/')  # 唯一url原则，带/可以兼容不带/情况
 def hello():
     return 'Hello World!'
 
 
-app.add_url_rule('/hello/', view_func=hello)  # 另一个注册路由方式
+# app.add_url_rule('/hello/', view_func=hello)  # 另一个注册路由方式
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81)
